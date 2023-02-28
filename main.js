ac90197bb1b80fe846cd093490bef67988e4d262
andreeeyyka <kolpakovandrey2007@gmail.com>
2023-02-28 13:51:15 +0300

    js burger
    burger finishhh

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-var smb = false
-
-
-function SlideMenuButton() {
-    smb = true
-    if (smb == true) {
-        document.queryselector(".btn").classlist.add(".header__burger_active")
+var menuclicked = false
+function burger() {
+    if (!menuclicked) {
+        menuclicked = true
+        document.querySelector(".slide-menu").style += 'display:block;'
+        document.querySelector(".slide-menu__wrapper").classList.add("slide-menu__wrapper_short")
+        document.querySelector(".burger-menu").style += 'display:block;'
+        document.querySelector(".catalog-menu").style += 'display:block;'
     } else {
-        document.queryselector(".btn").classlist.remove(".header__burger_active")
+        menuclicked = false
+        document.querySelector(".slide-menu").style= 'display:none;'
+        document.querySelector(".slide-menu__wrapper").classList.remove("slide-menu__wrapper_short")
+        document.querySelector(".burger-menu").style= 'display:none;'
+        document.querySelector(".catalog-menu").style= 'display:none;'
     }
 }
